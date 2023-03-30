5417cee5a0a8f104baa430465f5ea50ee174454d
firasglai <glaifiras44@gmail.com>
2023-03-31 00:02:55 +0100

    User Entity

--- src/main/java/com/spring/hospital/repository/UserRepository.java
+++ src/main/java/com/spring/hospital/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.spring.hospital.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    
+
 }
