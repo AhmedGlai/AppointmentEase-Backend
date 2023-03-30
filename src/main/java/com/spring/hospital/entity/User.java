64e90e220cdfee59f72530b97ba622b24206846c
firasglai <glaifiras44@gmail.com>
2023-03-30 23:58:03 +0100

    User Entity

--- src/main/java/com/spring/hospital/entity/User.java
+++ src/main/java/com/spring/hospital/entity/User.java
@@ -1,13 +1,63 @@
 package com.spring.hospital.entity;
 
 import com.spring.hospital.ennumeration.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User {
+import java.util.Collection;
+import java.util.List;
 
-    private String username;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_User")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String firstname;
+    private String lastname;
+    @Column(name = "user_email",nullable = false)
     private String email;
+    @Column(name = "user_email",nullable = false)
     private String password;
+    @Column(name = "user_email",nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return  List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
